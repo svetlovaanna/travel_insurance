e6b47660c26e2c1395e5c5d1701dae5562d96b0c
Anna Svetlova <yaannawhite@yandex.ru>
2025-06-02 20:47:09 +0300

    STEP 012 - DONE

--- core/src/main/java/org/javaguru/travel/insurance/rest/TravelCalculatePremiumResponse.java
+++ core/src/main/java/org/javaguru/travel/insurance/rest/TravelCalculatePremiumResponse.java
@@ -9,4 +9,11 @@ public class TravelCalculatePremiumResponse {
     private Date agreementDateFrom;
     private Date agreementDateTo;
 
+    public TravelCalculatePremiumResponse(String personFirstName, String personLastName,
+                                          Date agreementDateFrom, Date agreementDateTo) {
+        this.personFirstName = personFirstName;
+        this.personLastName = personLastName;
+        this.agreementDateFrom = agreementDateFrom;
+        this.agreementDateTo = agreementDateTo;
+    }
 }
