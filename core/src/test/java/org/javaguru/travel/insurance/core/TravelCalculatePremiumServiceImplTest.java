74f2fb5ad638f867b9aa0c39cdb6f73117324376
Anna Svetlova <191621076+svetlovaanna@users.noreply.github.com>
2025-06-04 21:09:26 +0300

    STEP 014 - DONE

--- core/src/test/java/org/javaguru/travel/insurance/core/TravelCalculatePremiumServiceImplTest.java
+++ core/src/test/java/org/javaguru/travel/insurance/core/TravelCalculatePremiumServiceImplTest.java
@@ -12,11 +12,37 @@ class TravelCalculatePremiumServiceImplTest {
 
     private final TravelCalculatePremiumServiceImpl service = new TravelCalculatePremiumServiceImpl();
     @Test
-    public void shouldPopulateResponse() {
+    public void shouldPopulateResponseFirstName() {
         TravelCalculatePremiumRequest request = new TravelCalculatePremiumRequest(
                 "Vasya", "Pupkin", new Date(), new Date());
-
         TravelCalculatePremiumResponse response = service.calculatePremium(request);
+        assertEquals(response.getPersonFirstName(), request.getPersonFirstName());
+    }
+    @Test
+    public void shouldPopulateResponseLastName() {
+        TravelCalculatePremiumRequest request = new TravelCalculatePremiumRequest(
+                "Vasya", "Pupkin", new Date(), new Date());
+        TravelCalculatePremiumResponse response = service.calculatePremium(request);
+        assertEquals(response.getPersonLastName(), request.getPersonLastName());
+    }
+    @Test
+    public void shouldPopulateResponseDateFrom() {
+        TravelCalculatePremiumRequest request = new TravelCalculatePremiumRequest(
+                "Vasya", "Pupkin", new Date(), new Date());
+        TravelCalculatePremiumResponse response = service.calculatePremium(request);
+        assertEquals(response.getAgreementDateFrom(), request.getAgreementDateFrom());
+    }
+    @Test
+    public void shouldPopulateResponseDateTo() {
+        TravelCalculatePremiumRequest request = new TravelCalculatePremiumRequest(
+                "Vasya", "Pupkin", new Date(), new Date());
+        TravelCalculatePremiumResponse response = service.calculatePremium(request);
+        assertEquals(response.getAgreementDateTo(), request.getAgreementDateTo());
+    }
+
+
+/*
+
 //TODO разбраться подробнее с фейл статусами
         //        request.setPersonFirstName("aaa");
         assertAll("Проверка всех свойств пользователя",
@@ -25,6 +51,5 @@ class TravelCalculatePremiumServiceImplTest {
                 () -> assertEquals(response.getAgreementDateFrom(), request.getAgreementDateFrom(), "Дата ОТ не совпадает"),
                 () -> assertEquals(response.getAgreementDateTo(), request.getAgreementDateTo(), "Дата ДО не совпадает")
         );
-    }
-
+*/
 }
