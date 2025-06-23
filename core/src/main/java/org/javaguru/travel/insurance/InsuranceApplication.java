69d96568430b84874a145464990b9f8caab570c5
Anna Svetlova <191621076+svetlovaanna@users.noreply.github.com>
2025-06-23 23:02:58 +0300

    try Qodana

--- core/src/main/java/org/javaguru/travel/insurance/InsuranceApplication.java
+++ core/src/main/java/org/javaguru/travel/insurance/InsuranceApplication.java
@@ -3,12 +3,10 @@ package org.javaguru.travel.insurance;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import java.util.ArrayList;
-
 @SpringBootApplication
 public class InsuranceApplication {
-    int unused = 10;
-    public static void main(String[] args) {
+
+        public static void main(String[] args) {
         SpringApplication.run(InsuranceApplication.class, args);
     }
 
--- core/src/main/java/org/javaguru/travel/insurance/rest/TravelCalculatePremiumRequest.java
+++ core/src/main/java/org/javaguru/travel/insurance/rest/TravelCalculatePremiumRequest.java
@@ -3,7 +3,6 @@ package org.javaguru.travel.insurance.rest;
 import lombok.Getter;
 import lombok.Setter;
 import java.util.Date;
-import java.text.ParseException;
 
 @Getter @Setter
 public class TravelCalculatePremiumRequest {
