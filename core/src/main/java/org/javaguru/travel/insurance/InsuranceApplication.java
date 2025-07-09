9e28f612ad1694ef1a538a108a4ce667662e0839
Anna Svetlova <191621076+svetlovaanna@users.noreply.github.com>
2025-07-09 22:49:35 +0300

    answer

--- core/src/main/java/org/javaguru/travel/insurance/InsuranceApplication.java
+++ core/src/main/java/org/javaguru/travel/insurance/InsuranceApplication.java
@@ -7,12 +7,20 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class InsuranceApplication {
 
         public static void main(String[] args) {
-            String question = """
-                    I wonder when I'll be able to configure 
-                    my brain to work 
-                    in ambiguity and uncertainty mode?
+            String think = """
+                    Seriousness? Seriously? That was my own old mistake. 
+                    I used to think seriousness meant effort, discipline, even self-violence. 
+                    As if the current approach 
+                    must be 
+                    precisely followed with all the prescribed rituals and methods. 
+                    
+                    Only the goal can be serious — and even then, not ‘serious’ but precise. 
+                    And that’s a big difference. 
+                    Achievement should feel playful and alive, just like an MVP — a dancing hypothesis. 
+                    
+                    Seriousness, as I saw it, was believing that my hypothesis was already 200% correct.
                     """;
-            System.out.println(question);
+            System.out.println(think);
             SpringApplication.run(InsuranceApplication.class, args);
     }
 
