99ae3255671ea7f6ee22d62b28bb9ce610304fe0
Anna Svetlova <191621076+svetlovaanna@users.noreply.github.com>
2025-07-16 22:40:21 +0300

    TASK test8

--- core/src/main/java/org/javaguru/travel/insurance/InsuranceApplication.java
+++ core/src/main/java/org/javaguru/travel/insurance/InsuranceApplication.java
@@ -8,17 +8,7 @@ public class InsuranceApplication {
 
         public static void main(String[] args) {
             String think = """
-                    Seriousness? Seriously? That was my own old mistake. 
-                    I used to think seriousness meant effort, discipline, even self-violence. 
-                    As if the current approach 
-                    must be 
-                    precisely followed with all the prescribed rituals and methods. 
                     
-                    Only the goal can be serious — and even then, not ‘serious’ but precise. 
-                    And that’s a big difference. 
-                    Achievement should feel playful and alive, just like an MVP — a dancing hypothesis. 
-                    
-                    Seriousness, as I saw it, was believing that my hypothesis was already 200% correct.
                     """;
             System.out.println(think);
             SpringApplication.run(InsuranceApplication.class, args);
