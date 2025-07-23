ddb5f0d2a5a6ffe02453a10d9cd77a1897f23d72
Anna Svetlova <191621076+svetlovaanna@users.noreply.github.com>
2025-07-23 21:52:30 +0300

    TASK 23 done

--- core/src/main/java/org/javaguru/travel/insurance/rest/TravelCalculatePremiumRequest.java
+++ core/src/main/java/org/javaguru/travel/insurance/rest/TravelCalculatePremiumRequest.java
@@ -8,7 +8,8 @@ import lombok.ToString;
 import java.util.Date;
 
 @Builder //(toBuilder = true)
-@Getter @Setter
+@Getter
+@Setter
 @ToString
 public class TravelCalculatePremiumRequest {
 
--- core/src/test/java/org/javaguru/travel/insurance/core/TravelCalculatePremiumServiceImplTest.java
+++ core/src/test/java/org/javaguru/travel/insurance/core/TravelCalculatePremiumServiceImplTest.java
@@ -3,7 +3,11 @@ package org.javaguru.travel.insurance.core;
 import org.javaguru.travel.insurance.mother.TravelCalculatePremiumServiceImplMother;
 import org.javaguru.travel.insurance.rest.TravelCalculatePremiumRequest;
 import org.javaguru.travel.insurance.rest.TravelCalculatePremiumResponse;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+
+import java.math.BigDecimal;
+
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 
@@ -13,34 +17,37 @@ class TravelCalculatePremiumServiceImplTest {
 
     private TravelCalculatePremiumRequest request;
     private DateTimeService dateTimeService;
-    private final TravelCalculatePremiumServiceImpl service = new TravelCalculatePremiumServiceImpl(dateTimeService);
+    private TravelCalculatePremiumServiceImpl service;
 
-    dateTimeService = mock(DateTimeService .class);
-
-    when(dateTimeService.calculateDays(request.getAgreementDateFrom(), request.getAgreementDateTo())).thenReturn(0L);
+    @BeforeEach
+    void setUp() {
+        request = TravelCalculatePremiumServiceImplMother.defailtValue();
+        dateTimeService = mock(DateTimeService.class);
+        when(dateTimeService.calculateDays(request.getAgreementDateFrom(), request.getAgreementDateTo())).thenReturn(BigDecimal.ZERO);
+        service = new TravelCalculatePremiumServiceImpl(dateTimeService);
+    }
 
     @Test
     void shouldPopulateResponseFirstName() {
-        TravelCalculatePremiumRequest request = TravelCalculatePremiumServiceImplMother.defailtValue();
         TravelCalculatePremiumResponse response = service.calculatePremium(request);
         System.out.println(request);
         assertEquals(response.getPersonFirstName(), request.getPersonFirstName());
     }
     @Test
     void shouldPopulateResponseLastName() {
-        TravelCalculatePremiumRequest request = TravelCalculatePremiumServiceImplMother.defailtValue();
+//        TravelCalculatePremiumRequest request = TravelCalculatePremiumServiceImplMother.defailtValue();
         TravelCalculatePremiumResponse response = service.calculatePremium(request);
         assertEquals(response.getPersonLastName(), request.getPersonLastName());
     }
     @Test
     void shouldPopulateResponseDateFrom() {
-        TravelCalculatePremiumRequest request = TravelCalculatePremiumServiceImplMother.defailtValue();
+//        TravelCalculatePremiumRequest request = TravelCalculatePremiumServiceImplMother.defailtValue();
         TravelCalculatePremiumResponse response = service.calculatePremium(request);
         assertEquals(response.getAgreementDateFrom(), request.getAgreementDateFrom());
     }
     @Test
     void shouldPopulateResponseDateTo() {
-        TravelCalculatePremiumRequest request = TravelCalculatePremiumServiceImplMother.defailtValue();
+//        TravelCalculatePremiumRequest request = TravelCalculatePremiumServiceImplMother.defailtValue();
         TravelCalculatePremiumResponse response = service.calculatePremium(request);
         assertEquals(response.getAgreementDateTo(), request.getAgreementDateTo());
     }
