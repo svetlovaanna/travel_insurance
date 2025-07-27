0b3502deb9564df4fa0553f19387158e315abd6a
Anna Svetlova <191621076+svetlovaanna@users.noreply.github.com>
2025-07-27 23:16:25 +0300

    TASK 27 done

--- core/src/main/java/org/javaguru/travel/insurance/core/TravelCalculatePremiumServiceImpl.java
+++ core/src/main/java/org/javaguru/travel/insurance/core/TravelCalculatePremiumServiceImpl.java
@@ -1,17 +1,16 @@
 package org.javaguru.travel.insurance.core;
 
+import lombok.AccessLevel;
+import lombok.RequiredArgsConstructor;
 import org.javaguru.travel.insurance.rest.TravelCalculatePremiumRequest;
 import org.javaguru.travel.insurance.rest.TravelCalculatePremiumResponse;
 import org.springframework.stereotype.Component;
 
 @Component
+@RequiredArgsConstructor(access = AccessLevel.PACKAGE)
 class TravelCalculatePremiumServiceImpl implements TravelCalculatePremiumService {
 
     private final DateTimeService dateTimeService;
-
-    TravelCalculatePremiumServiceImpl(DateTimeService dateTimeService) {
-        this.dateTimeService = dateTimeService;
-    }
 
     @Override
     public TravelCalculatePremiumResponse calculatePremium(TravelCalculatePremiumRequest request) {
